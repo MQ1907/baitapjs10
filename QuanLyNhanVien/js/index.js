c23c9bbd1b9fc8c8e185aa0732014dbdfdf860a7
minhquan <dinhbaminhquan69@gmail.com>
2023-07-11 13:13:07 +0700

    da xong bai

--- QuanLyNhanVien/js/index.js
+++ QuanLyNhanVien/js/index.js
@@ -2,35 +2,24 @@ let employees = [];
 
 //Thêm nhân viên
 function addEmployee() {
-  let account = document.getElementById("account").value;
-  let name = document.getElementById("name").value;
-  let email = document.getElementById("email").value;
-  let password = document.getElementById("password").value;
-  let dayWork = document.getElementById("dayWork").value;
-  let salary = +document.getElementById("salary").value;
-  let chucvu = document.getElementById("chucvu").value;
-  let timeWork = +document.getElementById("timeWork").value;
+  let employee = validate();
+  if (!employee) {
+    return;
+  }
 
-  let employee = new Employee(
-    account,
-    name,
-    email,
-    password,
-    dayWork,
-    salary,
-    chucvu,
-    timeWork
-  );
-
-let isValid = validate(employee)
-if(!isValid){
-  return
-}
-  
   employees.push(employee);
 
   display(employees);
   $("#myModal").modal("hide");
+  //Reset form
+  document.getElementById("account").value = "";
+  document.getElementById("name").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("password").value = "";
+  document.getElementById("dayWork").value = "";
+  document.getElementById("salary").value = "";
+  document.getElementById("chucvu").value = "";
+  document.getElementById("timeWork").value = "";
 }
 
 //Tìm kiếm nhân viên
@@ -105,6 +94,28 @@ function updateEmployee() {
 
   display(employees);
   $("#myModal").modal("hide");
+  //Reset form
+  document.getElementById("account").value = "";
+  document.getElementById("name").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("password").value = "";
+  document.getElementById("dayWork").value = "";
+  document.getElementById("salary").value = "";
+  document.getElementById("chucvu").value = "";
+  document.getElementById("timeWork").value = "";
+  document.getElementById("account").disabled = false;
+  document.getElementById("btnThemNV").disabled = false;
+}
+
+function closeEmployee() {
+  document.getElementById("tbTKNV").innerHTML = "";
+  document.getElementById("tbTen").innerHTML = "";
+  document.getElementById("tbEmail").innerHTML = "";
+  document.getElementById("tbMatKhau").innerHTML = "";
+  document.getElementById("tbNgay").innerHTML = "";
+  document.getElementById("tbLuongCB").innerHTML = "";
+  document.getElementById("tbChucVu").innerHTML = "";
+  document.getElementById("tbGiolam").innerHTML = "";
 }
 
 function display(employees) {
@@ -136,18 +147,17 @@ function display(employees) {
   document.getElementById("tableDanhSach").innerHTML = html;
 }
 
-
-function isRequired(value){
-if (!value.trim()){
-  return false
+function isRequired(value) {
+  if (!value.trim()) {
+    return false;
+  }
+  return true;
 }
-return true
-}
 
-//Kiểm tra có phải lương hay không 
+//Kiểm tra có phải lương hay không
 function isSalary(value) {
-  if (typeof value !== 'number'){
-    return false
+  if (typeof value !== "number") {
+    return false;
   }
   if (value < 1000000 || value > 20000000) {
     return false;
@@ -155,10 +165,10 @@ function isSalary(value) {
   return true;
 }
 
-//Kiểm tra thời gian làm 
+//Kiểm tra thời gian làm
 function isHours(value) {
-  if (typeof value !== 'number'){
-    return false
+  if (typeof value !== "number") {
+    return false;
   }
   if (value < 80 || value > 200) {
     return false;
@@ -166,52 +176,233 @@ function isHours(value) {
   return true;
 }
 
-
-
-
-function validate(employee){
-
-  let isValid = true
-
-if(!isRequired(employee.account)){
-  isValid = false
-  document.getElementById("tbTKNV").innerHTML= "Tài khoản không được để trống"
+//Điều kiện cho tài khoản
+function isAccount(value) {
+  // all characters with limited 4 - 6 length
+  let regex = /^.{4,6}$/;
+  return regex.test(value);
 }
 
-if(!isRequired(employee.name)){
-  isValid = false
-  document.getElementById("tbTen").innerHTML= "Tên không được để trống"
+//Điều kiện cho tên
+function isName(value) {
+  // letter a-z, A-Z without limited length. Accept spacing between word
+  let regex = /^[a-zA-Z]+( [a-zA-Z]+)*$/;
+  return regex.test(value);
 }
 
-if(!isRequired(employee.email)){
-  isValid = false
-  document.getElementById("tbEmail").innerHTML= "Email không được để trống"
+//Điều kiện cho email
+function isEmail(value) {
+  let regex = /^[\w\-\.]+@([\w-]+\.)+[\w-]{2,}$/;
+  return regex.test(value);
 }
 
-if(!isRequired(employee.password)){
-  isValid = false
-  document.getElementById("tbMatKhau").innerHTML= "Mật khẩu không được để trống"
+//Điều kiện cho password
+function isPassword(value) {
+  let regex =
+    /^(?=.*[A-Z])(?=.*[!&%\/()=\?\^\*\+\]\[#><;:,\._-|@])(?=.*[0-9])(?=.*[a-z]).{8,40}$/;
+
+  return regex.test(value);
 }
 
-if(!isRequired(employee.dayWork)){
-  isValid = false
-  document.getElementById("tbNgay").innerHTML= "Ngày làm không được để trống"
+function validate() {
+  let account = document.getElementById("account").value;
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+  let dayWork = document.getElementById("dayWork").value;
+  let salary = document.getElementById("salary").value;
+  let chucvu = document.getElementById("chucvu").value;
+  let timeWork = document.getElementById("timeWork").value;
+
+  let isValid = true;
+
+  if (!isRequired(account)) {
+    isValid = false;
+    document.getElementById("tbTKNV").innerHTML =
+      "Tài khoản không được để trống";
+    document.getElementById("tbTKNV").style.display = "block";
+  } else if (!isAccount(account)) {
+    document.getElementById("tbTKNV").innerHTML = "Tài khoản không hợp lệ";
+    document.getElementById("tbTKNV").style.display = "block";
+  } else {
+    document.getElementById("tbNgay").innerHTML = "";
+    document.getElementById("tbNgay").style.display = "none";
+  }
+
+  if (!isRequired(name)) {
+    isValid = false;
+    document.getElementById("tbTen").innerHTML = "Tên không được để trống";
+    document.getElementById("tbTen").style.display = "block";
+  } else if (!isName(name)) {
+    isValid = false;
+    document.getElementById("tbTen").innerHTML = "Tên không hợp lệ";
+    document.getElementById("tbTen").style.display = "block";
+  } else {
+    document.getElementById("tbTen").innerHTML = "";
+    document.getElementById("tbTen").style.display = "none";
+  }
+
+  if (!isRequired(email)) {
+    isValid = false;
+    document.getElementById("tbEmail").innerHTML = "Email không được để trống";
+    document.getElementById("tbEmail").style.display = "block";
+  } else if (!isEmail(email)) {
+    isValid = false;
+    document.getElementById("tbEmail").innerHTML = "Email không hợp lệ";
+    document.getElementById("tbEmail").style.display = "block";
+  } else {
+    document.getElementById("tbEmail").innerHTML = "";
+    document.getElementById("tbEmail").style.display = "none";
+  }
+
+  if (!isRequired(password)) {
+    isValid = false;
+    document.getElementById("tbMatKhau").innerHTML =
+      "Mật khẩu không được để trống";
+    document.getElementById("tbMatKhau").style.display = "block";
+  } else if (!isPassword(password)) {
+    isValid = false;
+    document.getElementById("tbMatKhau").innerHTML = "Mật khẩu không hợp lệ";
+    document.getElementById("tbMatKhau").style.display = "block";
+  } else {
+    document.getElementById("tbMatKhau").innerHTML = "";
+    document.getElementById("tbMatKhau").style.display = "none";
+  }
+
+  if (!isRequired(dayWork)) {
+    isValid = false;
+    document.getElementById("tbNgay").innerHTML =
+      "Ngày làm không được để trống";
+    document.getElementById("tbNgay").style.display = "block";
+  } else {
+    document.getElementById("tbNgay").innerHTML = "";
+    document.getElementById("tbNgay").style.display = "none";
+  }
+
+  let spanSalary = document.getElementById("tbLuongCB");
+  if (!isRequired(salary)) {
+    isValid = false;
+    spanSalary.innerHTML = "Lương không được để trống";
+    spanSalary.style.display = "block";
+  } else if (!isSalary(+salary)) {
+    isValid = false;
+    spanSalary.innerHTML = "Lương không hợp lệ";
+    spanSalary.style.display = "block";
+  } else {
+    spanSalary.innerHTML = "";
+    spanSalary.style.display = "none";
+  }
+
+  if (!isRequired(chucvu)) {
+    isValid = false;
+    document.getElementById("tbChucVu").innerHTML =
+      "Chức vụ không được để trống";
+    document.getElementById("tbChucVu").style.display = "block";
+  } else {
+    document.getElementById("tbChucVu").innerHTML = "";
+    document.getElementById("tbChucVu").style.display = "none";
+  }
+
+  let spanTime = document.getElementById("tbGiolam");
+  if (!isRequired(timeWork)) {
+    isValid = false;
+    spanTime.innerHTML = "Giờ làm không được để trống";
+    spanTime.style.display = "block";
+  } else if (!isHours(+timeWork)) {
+    isValid = false;
+    spanTime.innerHTML = "Giờ làm không hợp lệ";
+    spanTime.style.display = "block";
+  } else {
+    spanTime.innerHTML = "";
+    spanTime.style.display = "none";
+  }
+
+  if (isValid) {
+    let employee = new Employee(
+      account,
+      name,
+      email,
+      password,
+      dayWork,
+      +salary,
+      chucvu,
+      +timeWork
+    );
+    return employee;
+  }
+  return undefined;
 }
 
-if(!isSalary(employee.salary)){
-  isValid = false
-  document.getElementById("tbLuongCB").innerHTML= "Lương không được để trống"
-}
+document.getElementById("account").oninput = (event) => {
+  let idSpan = document.getElementById("tbTKNV");
+  if (isRequired(event.target.value)) {
+    idSpan.innerHTML = "";
+  } else {
+    idSpan.innerHTML = "Tài khoản không được để trống";
+  }
+};
 
-if(!isRequired(employee.chucvu)){
-  isValid = false
-  document.getElementById("tbChucVu").innerHTML= "Chức vụ không được để trống"
-}
+document.getElementById("name").oninput = (event) => {
+  let idSpan = document.getElementById("tbTen");
+  if (isRequired(event.target.value)) {
+    idSpan.innerHTML = "";
+  } else {
+    idSpan.innerHTML = "Tên không được để trống";
+  }
+  document.getElementById("btnThemNV").disabled = false;
+};
 
-if(!isHours(employee.timeWork)){
-  isValid = false
-  document.getElementById("tbGiolam").innerHTML= "Giờ làm không được để trống"
-}
+document.getElementById("email").oninput = (event) => {
+  let idSpan = document.getElementById("tbEmail");
+  if (isRequired(event.target.value)) {
+    idSpan.innerHTML = "";
+  } else {
+    idSpan.innerHTML = "Email không được để trống";
+  }
+};
 
-return isValid
-}
+document.getElementById("password").oninput = (event) => {
+  let idSpan = document.getElementById("tbMatKhau");
+  if (isRequired(event.target.value)) {
+    idSpan.innerHTML = "";
+  } else {
+    idSpan.innerHTML = "Mật khẩu không được để trống";
+  }
+};
+
+document.getElementById("dayWork").oninput = (event) => {
+  let idSpan = document.getElementById("tbNgay");
+  if (isRequired(event.target.value)) {
+    idSpan.innerHTML = "";
+  } else {
+    idSpan.innerHTML = "Ngày không được để trống";
+  }
+};
+
+document.getElementById("salary").oninput = (event) => {
+  let idSpan = document.getElementById("tbLuongCB");
+  if (isRequired(event.target.value)) {
+    idSpan.innerHTML = "";
+  } else {
+    idSpan.innerHTML = "Lương không được để trống";
+  }
+};
+
+document.getElementById("chucvu").oninput = (event) => {
+  let idSpan = document.getElementById("tbChucVu");
+  if (isRequired(event.target.value)) {
+    idSpan.innerHTML = "";
+  } else {
+    idSpan.innerHTML = "Chức vụ không được để trống";
+  }
+};
+
+document.getElementById("timeWork").oninput = (event) => {
+  console.log("he");
+  let idSpan = document.getElementById("tbGiolam");
+  if (isRequired(event.target.value)) {
+    idSpan.innerHTML = "";
+  } else {
+    idSpan.innerHTML = "Giờ làm không được để trống";
+  }
+};
